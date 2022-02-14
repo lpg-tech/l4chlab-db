d5a4ea955cd42f850388782fc123ef5263cab5f8
lpg-tech <lpg@ganimedes.ch>
2022-02-14 16:13:44 +0100

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+Rails.application.routes.draw do
+  get 'faq', to: 'pages#faq'
+  get 'about', to: 'pages#about'
+  root 'pages#home'
+  get 'pages/home'
+  get 'pages/about'
+  get 'pages/faq'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
